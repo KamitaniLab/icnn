f3d492945202d9fbd5d65ade51f7953617cb225a
Shuntaro Aoki <aoki@atr.jp>
2018-06-29 12:21:56 +0900

    Update comments, style fix

--- recon_img_from_true_feature_icnn_gd.py
+++ recon_img_from_true_feature_icnn_gd.py
@@ -1,9 +1,12 @@
-# demonstration codes for the usage of "icnn_gd"
-# the codes will do the following:
-# 1 extract cnn features from a test image;
-# 2 reconstruct the test image from the CNN features;
+'''Demonstration code for icnn_gd
 
-# import
+This script will do the followings:
+
+1. extract cnn features from a test image,
+2. reconstruct the test image from the CNN features.
+'''
+
+
 import os
 import pickle
 from datetime import datetime
@@ -14,16 +17,19 @@ import scipy.io as sio
 from scipy.misc import imresize
 
 import caffe
+
 from icnn.icnn_gd import reconstruct_image
 from icnn.utils import get_cnn_features, normalise_img
 
-# average image of ImageNet
+
+# Setup Caffe CNN model -------------------------------------------------------
+
+# Load the average image of ImageNet
 img_mean_fn = './data/ilsvrc_2012_mean.npy'
 img_mean = np.load(img_mean_fn)
-img_mean = np.float32(
-    [img_mean[0].mean(), img_mean[1].mean(), img_mean[2].mean()])
+img_mean = np.float32([img_mean[0].mean(), img_mean[1].mean(), img_mean[2].mean()])
 
-# load cnn model
+# Load CNN model
 model_file = './net/VGG_ILSVRC_19_layers/VGG_ILSVRC_19_layers.caffemodel'
 prototxt_file = './net/VGG_ILSVRC_19_layers/VGG_ILSVRC_19_layers.prototxt'
 channel_swap = (2, 1, 0)
@@ -32,106 +38,131 @@ net = caffe.Classifier(prototxt_file, model_file,
 h, w = net.blobs['data'].data.shape[-2:]
 net.blobs['data'].reshape(1, 3, h, w)
 
-# layer list
+# Layer list: using all conv and fc layers
 # layer_list = ['conv1_1','conv2_1','conv3_1']
 layer_list = []
 for layer in net.blobs.keys():
-    if 'conv' in layer or 'fc' in layer:  # use all conv and fc layers
+    if 'conv' in layer or 'fc' in layer:
         layer_list.append(layer)
 
-# make directory for saving the results
+# Setup directories -----------------------------------------------------------
+
+# Make directory for saving the results
 save_dir = './result'
 save_folder = __file__.split('.')[0]
 save_folder = save_folder + '_' + datetime.now().strftime('%Y%m%dT%H%M%S')
 save_path = os.path.join(save_dir, save_folder)
 os.mkdir(save_path)
 
-# original image
+# Setup the test image and image features -------------------------------------
+
+# Test image
 orig_img = PIL.Image.open('./data/orig_img.jpg')
 
-# resize the image to match the input size of the CNN model
+# Resize the image to match the input size of the CNN model
 orig_img = imresize(orig_img, (h, w), interp='bicubic')
 
-# extract CNN features from the original image
+# Extract CNN features from the test image
 features = get_cnn_features(net, orig_img, layer_list)
 
-# save original image
+# Save the test image
 save_name = 'orig_img.jpg'
 PIL.Image.fromarray(orig_img).save(os.path.join(save_path, save_name))
 
-# weight of each layer in the total loss function
+# Setup layer weights (optional) ----------------------------------------------
+
+# Weight of each layer in the total loss function
 num_of_layer = len(layer_list)
 feat_norm_list = np.zeros(num_of_layer, dtype='float32')
 for j, layer in enumerate(layer_list):
-    # norm of the CNN features for each layer
+    # Norm of the CNN features for each layer
     feat_norm_list[j] = np.linalg.norm(features[layer])
-# use the inverse of the squared norm of the CNN features as the weight for each layer
+# Use the inverse of the squared norm of the CNN features as the weight for each layer
 weights = 1. / (feat_norm_list**2)
-# normalise the weights such that the sum of the weights = 1
+# Normalise the weights such that the sum of the weights = 1
 weights = weights / weights.sum()
 layer_weight = {}
 for j, layer in enumerate(layer_list):
     layer_weight[layer] = weights[j]
 
-# reconstruction options
+# Reconstruction --------------------------------------------------------------
+
+# Reconstruction options
 opts = {
+    # Loss function type: {'l2', 'l1', 'inner', 'gram'}
+    'loss_type': 'l2',
 
-    'loss_type': 'l2',  # the loss function type: {'l2','l1','inner','gram'}
+    # The total number of iterations for gradient descend
+    'iter_n': 3,
 
-    'iter_n': 3,  # the total number of iterations for gradient descend
+    # Display the information on the terminal for every n iterations
+    'disp_every': 1,
 
-    'disp_every': 1,  # display the information on the terminal for every n iterations
-
-    'save_intermediate': True,  # save the intermediate reconstruction or not
-    # save the intermediate reconstruction for every n iterations
+    # Save the intermediate reconstruction or not
+    'save_intermediate': True,
+    # Save the intermediate reconstruction for every n iterations
     'save_intermediate_every': 10,
-    # the path to save the intermediate reconstruction
+    # Path to the directory saving the intermediate reconstruction
     'save_intermediate_path': save_path,
 
-    'lr_start': 2.,  # learning rate
+    # Learning rate
+    'lr_start': 2.,
     'lr_end': 1e-10,
 
-    'momentum_start': 0.9,  # gradient with momentum
+    # Gradient with momentum
+    'momentum_start': 0.9,
     'momentum_end': 0.9,
 
-    'decay_start': 0.2,  # pixel decay for each iteration
+    # Pixel decay for each iteration
+    'decay_start': 0.2,
     'decay_end': 1e-10,
 
-    'image_blur': True,  # Use image smoothing or not
-    'sigma_start': 2.,  # the size of the gaussian filter for image smoothing
+    # Use image smoothing or not
+    'image_blur': True,
+    # The size of the gaussian filter for image smoothing
+    'sigma_start': 2.,
     'sigma_end': 0.5,
 
-    # a python dictionary consists of weight parameter of each layer in the loss function, arranged in pairs of layer name (key) and weight (value);
+    # A python dictionary consists of weight parameter of each layer in the
+    # loss function, arranged in pairs of layer name (key) and weight (value);
     'layer_weight': layer_weight,
 
-    # the initial image for the optimization (setting to None will use random noise as initial image)
+    # The initial image for the optimization (setting to None will use random
+    # noise as initial image)
     'initial_image': None,
 
-    # a python dictionary consists of channels to be selected, arranged in pairs of layer name (key) and channel numbers (value); the channel numbers of each layer are the channels to be used in the loss function; use all the channels if some layer not in the dictionary; setting to None for using all channels for all layers;
+    # A python dictionary consists of channels to be selected, arranged in
+    # pairs of layer name (key) and channel numbers (value); the channel
+    # numbers of each layer are the channels to be used in the loss function;
+    # use all the channels if some layer not in the dictionary; setting to None
+    # for using all channels for all layers;
     'channel': None,
-    # a python dictionary consists of masks for the traget CNN features, arranged in pairs of layer name (key) and mask (value); the mask selects units for each layer to be used in the loss function (1: using the uint; 0: excluding the unit); mask can be 3D or 2D numpy array; use all the units if some layer not in the dictionary; setting to None for using all units for all layers;
-    'mask': None,
 
+    # A python dictionary consists of masks for the traget CNN features,
+    # arranged in pairs of layer name (key) and mask (value); the mask selects
+    # units for each layer to be used in the loss function (1: using the uint;
+    # 0: excluding the unit); mask can be 3D or 2D numpy array; use all the
+    # units if some layer not in the dictionary; setting to None for using all
+    #units for all layers;
+    'mask': None,
 }
 
-# save the optional parameters
+# Save the optional parameters
 save_name = 'options.pkl'
 with open(os.path.join(save_path, save_name), 'w') as f:
     pickle.dump(opts, f)
     f.close()
 
-# reconstruction
+# Reconstruction
 recon_img, loss_list = reconstruct_image(features, net, **opts)
 
-# save the results
+# Save the results ------------------------------------------------------------
+
 save_name = 'recon_img' + '.mat'
 sio.savemat(os.path.join(save_path, save_name), {'recon_img': recon_img})
 
 save_name = 'recon_img' + '.jpg'
-PIL.Image.fromarray(normalise_img(recon_img)).save(
-    os.path.join(save_path, save_name))
+PIL.Image.fromarray(normalise_img(recon_img)).save(os.path.join(save_path, save_name))
 
 save_name = 'loss_list' + '.mat'
 sio.savemat(os.path.join(save_path, save_name), {'loss_list': loss_list})
-
-# end
