503d3079b8956ea826a6539b11c0407a2eecd92e
Shuntaro Aoki <aoki@atr.jp>
2018-08-01 22:56:15 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+'''Setup script for icnn.'''
+
+
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(name='icnn',
+          version='1.0',
+          description='Inverting CNN',
+          author='Shen Guo-Hua, Kei Majima',
+          author_email='kamitanilab.contact@gmail.com',
+          url='https://github.com/KamitaniLab/icnn',
+          license='MIT',
+          packages=['icnn'],
+          install_requires=['numpy', 'scipy'])
